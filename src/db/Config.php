943685488e6204f600e0bc11b037a37affe67c5e
Shehab Ibrahim <sai6@njit.edu>
2019-04-18 12:50:07 -0400

    SQLite init

---
+++ src/db/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+
+class Config
+{
+    const PATH_TO_SQLITE_FILE = '../data/database.sqlite';
+}
+
--- src/index.php
+++ src/index.php
@@ -13,8 +13,27 @@ require_once '../vendor/autoload.php';
 <body>
 <?php
 
+use ShehabIbrahim\db\SQLConnection;
+/*require 'vendor/autoload.php';*/
 
-        new bootstrap('../data/file.csv');
+use App\SQLiteConnection;
+
+$pdo = (new SQLiteConnection())->connect();
+if ($pdo != null)
+    echo 'Connected to the SQLite database successfully!';
+else
+    echo 'Whoops, could not connect to the SQLite database!';
+
+$stmt = $pdo->prepare('SELECT * FROM contacts');
+$stmt->execute();
+$contacts = $stmt->fetchObject();
+
+print_r($contacts)
+/*
+new bootstrap('../data/file.csv');
+
+phpinfo();
+*/
 ?>
 </body>
 </html>
