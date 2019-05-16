7570ab8d66aad768e14da360ec09b281eac189ff
Aditya <ab946@njit.edu>
2019-05-16 12:01:33 -0400

    Modified the CodeTest file

--- test/CodeTest.php
+++ test/CodeTest.php
@@ -11,11 +11,6 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 final class CodeTest extends TestCase
 {
-    public function testCSVFileExists()
-    {
-        $this->assertFileExists('data/shoes.csv');
-    }
-
     public function testrecordFactorFile()
     {
         echo file_exists('recordFactor');
